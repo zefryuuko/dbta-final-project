1e8add6ac52182ede3d1b7c6adbbae5fd859856d
David Amadeo <43334264+davidamadeo@users.noreply.github.com>
2019-12-17 16:33:29 +0700

    2 shit

--- src-frontend/components/navbar_staff.php
+++ src-frontend/components/navbar_staff.php
@@ -1,34 +1,29 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="#">
-            <img src="../Images/starbucks favicon.png" width="85" height="85" class="d-inline-block align-top" alt="" />
+            <img src="/resources/logo.png" width="85" height="85" class="d-inline-block align-top" alt="" />
         </a>
-        <a class="navbar-brand" href="#" style="font-size:35px; font-weight: bold;">Home</a>
+        <a class="navbar-brand" href="#" style="font-size:20px; font-weight: bold;">Home</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand" href="#" style="font-size:35px; padding: 10px; font-weight: bold;">Transaction History</a>
+        <a class="navbar-brand" href="#" style="font-size:20px; padding: 10px; font-weight: bold;">Transaction History</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand" href="#" style="font-size:35px; padding: 10px; font-weight: bold;"></a>
+        <a class="navbar-brand" href="#" style="font-size:20px; padding: 10px; font-weight: bold;">New Order</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand" href="#" style="font-size:35px; padding: 10px; font-weight: bold;">New Order</a>
+        <a class="navbar-brand" href="#" style="font-size:20px; padding: 10px; font-weight: bold;">Staff Details</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand" href="#" style="font-size:35px; padding: 10px; font-weight: bold;">Staff Details</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <a class="navbar-brand" href="#" style="font-size:35px; padding: 10px; font-weight: bold;">Menu</a>
+        <a class="navbar-brand" href="#" style="font-size:20px; padding: 10px; font-weight: bold;">Menu</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -37,7 +32,7 @@
             <ul class="navbar-nav"></ul>
         </div>
         <a class="navbar-brand" href="#" style="margin-right:30px; margin-top: 10px;">
-            <img src="../Images/avatar.png" width="40" height="40" class="d-inline-block align-top" alt="" />
+            <img src="/resources/avatar.png" width="40" height="40" class="d-inline-block align-top" alt="" />
             <p style="float: right; padding: 5px; margin-top: 5px; font-size: 15px;">
                 Welcome back, Zef!
             </p>
@@ -48,7 +43,7 @@
 
 <div class="box" style="width: 1425px;
       height: 5px;
-      margin: 15px auto auto auto;
+      margin: 20px auto auto auto;
       background-color: #006335;
       border-radius: 5px;"></div>
 <!--Border Line-->
