306a7db461532eca08b20d58ce452e180204edec
Zef <titotobing0511@gmail.com>
2019-12-17 15:52:27 +0700

    Add generic navbar

---
+++ src-frontend/components/navbar.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar-light bg-white">
+      <a class="navbar-brand" href="#">
+        <img src="../Images/starbucks favicon.png" width="85" height="85" class="d-inline-block align-top" alt="" />
+        <p style="float: right; padding: 5px; margin-top: 9px; margin-left:10px; font-size: 35px; font-weight:bold;">
+          DASHBOARD
+        </p>
+      </a>
+    </nav>
+  </header>
+  <!--End of Header-->
+
+  <div class="box" style="width: 1425px;
+      height: 5px;
+      margin: 15px auto auto auto;
+      background-color: #006335;
+      border-radius: 5px;">
+  </div>
