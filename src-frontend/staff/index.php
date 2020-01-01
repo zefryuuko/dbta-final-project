6f057894d3d159d8a65c4dddd20a4c24ce1b2915
Zef <titotobing0511@gmail.com>
2020-01-02 00:17:40 +0700

    Add crude auth method

--- src-frontend/staff/index.php
+++ src-frontend/staff/index.php
@@ -7,9 +7,10 @@
     <link href="/resources/logo.png" rel="shortcut icon" />
     <title>Dashboard</title>
     <?php include "../components/bootstrap.php";?>
+    <?php include("auth.php");?>
   </head>
 
-  <body>
+  <body onload="auth()">
     <div class="container">
       <?php include "../components/navbar/navbar_welcome.php";?>
 
