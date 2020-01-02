f8f49f7af8f35444aae9b9213f305290bd182cbe
Zef <titotobing0511@gmail.com>
2020-01-02 14:24:39 +0700

    Bugfixes

--- src-frontend/staff/auth.php
+++ src-frontend/staff/auth.php
@@ -6,8 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") // Handle auth check
     if (isset($_COOKIE["id"]) && isset($_COOKIE["session"]))
     {
         $status = json_decode(isAuthenticated($pageLevel, $_COOKIE["id"], $_COOKIE["session"]));
-        if ($status["status"] == "failed")
+        if ($status->status == "failed")
             echo "<script>function auth(){alert('You are not authenticated to open this page.');window.location.replace('/staff/login.php');}</script>";
+    }
+    else
+    {
+        echo "<script>function auth(){window.location.replace('/staff/login.php');}</script>";
     }
 }
 else if ($_SERVER["REQUEST_METHOD"] == "POST") // Handle authentication
