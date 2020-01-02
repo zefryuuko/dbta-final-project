86dd451b61acdfc968538f4f927ff718800b0b80
Zef <titotobing0511@gmail.com>
2020-01-02 23:18:07 +0700

    Add edit modal generation

--- src-frontend/components/modular/membership_staff.php
+++ src-frontend/components/modular/membership_staff.php
@@ -6,7 +6,7 @@ function generateTableBody($currentPage = 1)
     $cards = getCards($currentPage);
     foreach ($cards as $card)
     {
-        echo "<tr><td>".$card["card_number"]."</td><td>".$card["cardholder_name"]."</td><td>".$card["card_balance"]."</td><td><a href=\"#\"><img src=\"/resources/cross.svg\" style=\"width: 45%\"></a></td><td><a href=\"#\"><img src=\"/resources/edit.svg\" style=\"width: 45%\"></a></td>";
+        echo "<tr><td>".$card["card_number"]."</td><td>".$card["cardholder_name"]."</td><td>".$card["card_balance"]."</td><td><a href=\"#\"><img src=\"/resources/cross.svg\" style=\"width: 45%\"></a></td><td><a data-target=\"#editModal".$card["card_number"]."\" data-toggle=\"modal\" href=\"#editModal".$card["card_number"]."\"><img src=\"/resources/edit.svg\" style=\"width: 45%\"></a></td>";
     }
 }
 
@@ -41,4 +41,65 @@ function generateTableBodyByName($name, $currentPage = 1)
     }
 }
 
+
+function generateModals($currentPage = 1)
+{
+    $cards = getCards($currentPage);
+    foreach ($cards as $card)
+    {
+        echo "<div
+        class=\"modal fade\"
+        id=\"editModal".$card["card_number"]."\"
+        tabindex=\"-1\"
+        role=\"dialog\"
+        aria-labelledby=\"exampleModalLabel\"
+        aria-hidden=\"true\"
+      >
+        <div class=\"modal-dialog\" role=\"document\">
+          <div class=\"modal-content\">
+            <div class=\"modal-header\">
+              <h5 class=\"modal-title\" id=\"exampleModalLabel\">Edit Membership</h5>
+              <button
+                type=\"button\"
+                class=\"close\"
+                data-dismiss=\"modal\"
+                aria-label=\"Close\"
+              >
+                <span aria-hidden=\"true\">&times;</span>
+              </button>
+            </div>
+            <div class=\"modal-body\">
+              <form action=\"/staff/membership.php\" method=\"POST\">
+                <input type=\"hidden\" class=\"form-control\" name=\"card_number\" value=\"".$card["card_number"]."\"/>
+                <div class=\"form-group\">
+                  <label for=\"branch-name\" class=\"col-form-label\"
+                    >Card Holder Name</label
+                  >
+                  <input type=\"text\" class=\"form-control\" name=\"cardholder_name\" value=\"".$card["cardholder_name"]."\"/>
+                </div>
+                <div class=\"form-group\">
+                  <label for=\"message-text\" class=\"col-form-label\"
+                    >Balance</label
+                  >
+                  <input type=\"text\" class=\"form-control\" name=\"card_balance\" value=\"".$card["card_balance"]."\"/>
+                </div>
+                <div class=\"modal-footer\">
+                  <button
+                    type=\"button\"
+                    class=\"btn btn-secondary\"
+                    data-dismiss=\"modal\"
+                  >
+                    Close
+                  </button>
+                  <button type=\"submit\" class=\"btn btn-primary\">
+                    Save changes
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>";
+    }
+}
 ?>
