be21ee436d7519ac5d92533e5e620e068099a085
Zef <titotobing0511@gmail.com>
2020-01-02 14:29:05 +0700

    Move login handler code

--- src-frontend/staff/auth.php
+++ src-frontend/staff/auth.php
@@ -14,26 +14,4 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") // Handle auth check
         echo "<script>function auth(){window.location.replace('/staff/login.php');}</script>";
     }
 }
-else if ($_SERVER["REQUEST_METHOD"] == "POST") // Handle authentication
-{
-    if (!empty($_POST["id"]) && !empty($_POST["pass"]))
-    {
-        $status = json_decode(authenticate($_POST["level"], $_POST["id"], md5($_POST["pass"])), true);
-        if ($status["status"] == "success")
-        {
-            // Set cookie
-            setcookie("id", $_POST["id"], time() + (86400 * 30), "/");
-            setcookie("session", $status["session"], time() + (86400 * 30), "/");
-            echo "<body onload=\"auth()\"><script>function auth(){window.location.replace('/staff');}</script>";
-        }
-        else
-        {
-            echo "<body onload=\"auth()\"><script>function auth(){alert('".$status["message"]."');window.location.replace('/staff/login.php');}</script>";
-        }
-    }
-    else 
-    {
-        echo "<script>function auth(){alert('Incorrect ID and or password.');window.location.replace('/staff/login.php');}</script>";
-    }
-}
 $username = !empty($_GET["page"]);
