b07ecf6527bf3cc593a4e3330cc7923bd686829e
Zef <titotobing0511@gmail.com>
2020-01-02 14:23:28 +0700

    Temporary fix

--- src-frontend/components/navbar/navbar_welcome.php
+++ src-frontend/components/navbar/navbar_welcome.php
@@ -7,14 +7,15 @@
             </p>
         </a>
         <ul class="navbar-nav" style="float: right; margin-right: 8px; margin-top:-2px;">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item">
+                <a class="nav-link" href="#" id="nav_item disabled" role="button" aria-haspopup="true" aria-expanded="false">
                     <img src="/resources/avatar.svg" width="40" height="40" class="rounded-circle">
+                    <span>Welcome back, <?php echo $staffName ?></span>
+                    <a style="float:right" href="/logout.php">Log Out</a>
                 </a>
-                <div class="dropdown-menu" style=" width: 10px !important;" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="index.php">Log Out</a>
-                </div>
             </li>
+            <!-- <li class="nav-item">
+            </li> -->
         </ul>
     </nav>
 </header>
