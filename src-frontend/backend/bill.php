48325120e0753b4de45939d512930d5167bf0f30
Zef <titotobing0511@gmail.com>
2020-01-02 15:14:50 +0700

    Create bill.php

---
+++ src-frontend/backend/bill.php
@@ -0,0 +1,11 @@
+<?php
+include_once("curl.php");
+
+$showCount = 5;
+
+function getBills($page = 1)
+{
+    global $showCount;
+    $raw_data = callAPI("GET", "http://localhost:8081/bill?count=".$showCount."&page=".$page);
+    return json_decode($raw_data, true);
+}
