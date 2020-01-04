36bf33dd6cfb88e17380cfcdf2fe8d1df1e49ec7
Zef <titotobing0511@gmail.com>
2020-01-04 19:37:09 +0700

    Add delete modal on search by name

--- src-frontend/components/modular/membership_staff.php
+++ src-frontend/components/modular/membership_staff.php
@@ -37,7 +37,7 @@ function generateTableBodyByName($name, $currentPage = 1)
     $cards = getCardsByName($name, $currentPage);
     foreach ($cards as $card)
     {
-        echo "<tr><td>".$card["card_number"]."</td><td>".$card["cardholder_name"]."</td><td>".$card["card_balance"]."</td><td><a href=\"#\"><img src=\"/resources/cross.svg\" style=\"width: 45%\"></a></td><td><a data-target=\"#editModal".$card["card_number"]."\" data-toggle=\"modal\" href=\"#editModal".$card["card_number"]."\"><img src=\"/resources/edit.svg\" style=\"width: 45%\"></a></td>";
+      echo "<tr><td>".$card["card_number"]."</td><td>".$card["cardholder_name"]."</td><td>".$card["card_balance"]."</td><td><a data-target=\"#deleteModal".$card["card_number"]."\" data-toggle=\"modal\" href=\"#deleteModal".$card["card_number"]."\"><img src=\"/resources/cross.svg\" style=\"width: 45%\"></a></td><td><a data-target=\"#editModal".$card["card_number"]."\" data-toggle=\"modal\" href=\"#editModal".$card["card_number"]."\"><img src=\"/resources/edit.svg\" style=\"width: 45%\"></a></td>";
     }
 }
 
@@ -212,6 +212,55 @@ function generateModalsByName($name)
           </div>
         </div>
       </div>";
+
+           // Delete Modal
+           echo "<div
+           class=\"modal fade\"
+           id=\"deleteModal".$card["card_number"]."\"
+           tabindex=\"-1\"
+           role=\"dialog\"
+           aria-labelledby=\"exampleModalLabel\"
+           aria-hidden=\"true\"
+         >
+           <div class=\"modal-dialog\" role=\"document\">
+             <div class=\"modal-content\">
+               <div class=\"modal-header\">
+                 <h5 class=\"modal-title\" id=\"exampleModalLabel\">Delete Member</h5>
+                 <button
+                   type=\"button\"
+                   class=\"close\"
+                   data-dismiss=\"modal\"
+                   aria-label=\"Close\"
+                 >
+                   <span aria-hidden=\"true\">&times;</span>
+                 </button>
+               </div>
+               <div class=\"modal-body\">
+                 <form action=\"/staff/membership.php\" method=\"GET\">
+                   <input type=\"hidden\" class=\"form-control\" name=\"delete\" value=\"yes\"/>
+                   <input type=\"hidden\" class=\"form-control\" name=\"card_number\" value=\"".$card["card_number"]."\"/>
+                   <div class=\"form-group\">
+                     <label for=\"branch-name\" class=\"col-form-label\"
+                       >Are you sure you want to delete ".$card["card_number"]." - ".$card["cardholder_name"]."</label
+                     >
+                   </div>
+                   <div class=\"modal-footer\">
+                     <button
+                       type=\"button\"
+                       class=\"btn btn-secondary\"
+                       data-dismiss=\"modal\"
+                     >
+                       Cancel
+                     </button>
+                     <button type=\"submit\" class=\"btn btn-danger\">
+                       Delete
+                     </button>
+                   </div>
+                 </form>
+               </div>
+             </div>
+           </div>
+         </div>";
     }
 }
 
