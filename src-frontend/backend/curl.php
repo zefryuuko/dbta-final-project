0a51aabee4accb381ef122f19a0dd85f61537124
Zef <titotobing0511@gmail.com>
2020-01-04 20:48:54 +0700

    Remove temporary code

--- src-frontend/backend/curl.php
+++ src-frontend/backend/curl.php
@@ -11,14 +11,6 @@ function callAPI($method, $url, $data = false)
             if ($data)
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
             break;
-        case "POSTJ":
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);                                                                  
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);                                                                      
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
-                'Content-Type: application/json',                                                                                
-                'Content-Length: ' . strlen($data))                                                                       
-            );
         case "PUT":
             curl_setopt($curl, CURLOPT_PUT, 1);
             break;
