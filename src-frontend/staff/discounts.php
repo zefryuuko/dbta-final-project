a9e5d499180ba2d4701d3b825193e3b0775793a3
Zef <titotobing0511@gmail.com>
2020-01-05 14:44:18 +0700

    Implement discounts page

--- src-frontend/staff/discounts.php
+++ src-frontend/staff/discounts.php
@@ -1,3 +1,46 @@
+<?php
+include_once("../backend/discount.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") // Branch Add
+{
+    if (!empty($_POST["add"]))
+    {
+      $status = addDiscount($_POST["discount_name"], $_POST["discount_percentage"]);
+        if ($status["status"] == "success")
+        {
+            echo "<script>function discount(){alert('Added discount successfully.');window.location.replace('/staff/discounts.php');}</script>";
+        }
+        else
+        {
+            echo "<script>function discount(){alert('".$status["message"]."');window.location.replace('/staff/discounts.php');}</script>";
+        }
+    }
+    else if (!empty($_POST["discount_name"]) && !empty($_POST["discount_percentage"]))
+    {
+        $status = updateDiscount($_POST["discount_id"], $_POST["discount_name"], $_POST["discount_percentage"]);
+        if ($status["status"] == "success")
+        {
+            echo "<script>function discount(){alert('Updated discount successfully.');window.location.replace('/staff/discounts.php');}</script>";
+        }
+        else
+        {
+            echo "<script>function discount(){alert('".$status["message"]."');window.location.replace('/staff/discounts.php');}</script>";
+        }
+    }
+    else 
+    {
+        echo "<script>function discount(){alert('Incorrect input.');window.location.replace('/staff/discounts.php');}</script>";
+    }
+}
+else  // Branch deletion
+{
+    if (!empty($_GET["delete"]) && !empty($_GET["discount_id"]))
+    {
+      removeDiscount($_GET["discount_id"]);
+      echo "<script>function discount(){window.location.replace('/staff/discounts.php');}</script>";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,12 +49,131 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link href="/resources/logo.png" rel="shortcut icon" />
     <title>Dashboard</title>
-    <?php include ("../components/bootstrap.php");?>
-  </head>
+    <?php include ("../components/bootstrap.php");?></script>
+    <?php 
+      $pageLevel = 0;
+      include("auth.php");
+    ?>
+</head>
 
-  <body>
+<body onload="try {auth()}catch(e){} discount()">
     <div class="container">
-      <!-- Button trigger modal -->
+      <?php include ("../components/navbar/navbar_staff.php");?>
+      <div class="bodyLeft">
+        <nav
+          class="navbar navbar-light"
+          style=""
+        >
+          <a class="navbar-brand" style="font-size:25px; font-weight: bold;"
+            >Discounts</a
+          >
+          <form action="/staff/discounts.php" method="GET"
+            class="form-inline"
+            style=""
+          >
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              name="name"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              class="btn btn-light btn-outline-dark my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </nav>
+
+        <div class="table-responsive">
+          <table
+            class="table"
+            style=""
+          >
+            <thead class="thead-dark" style="font-size: 20px">
+              <tr>
+                <th scope="col" style="width: 70px">ID</th>
+                <th scope="col" style="width: 250px">Discount Name</th>
+                <th scope="col" style="width: 250px">Percentage</th>
+                <th scope="col" style="width: 25px"></th>
+                <th scope="col" style="width: 25px"></th>
+              </tr>
+            </thead>
+            <tbody style="font-size: 18px">
+            <?php include ("../components/modular/discounts_staff.php");?>
+            <?php
+                // Searching implementation
+                if (!empty($_GET["name"]) ) generateTableBodyByName($_GET["name"], !empty($_GET["page"]) ? $_GET["page"] : 1);
+                else generateTableBody(!empty($_GET["page"]) ? $_GET["page"] : 1);
+            ?>
+          </tbody>
+          </table>
+        </div>
+      </div>
+      <nav aria-label="Page navigation">
+            <ul class="pagination">
+                <?php if (empty($_GET["name"])) generatePagination(!empty($_GET["page"]) ? $_GET["page"] : 1) ?>
+            </ul>
+        </nav>
+      <div
+        class="bodyRight"
+        style=" margin-top: 30px;"
+      >
+        <div
+          class="box"
+          style="width: 400px;
+                height: 450px;
+                padding: 40px;
+                border-radius: 5px;
+                text-align: center;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
+        >
+          <p
+            style="font-size:25px; padding: 10px; font-weight: bold; margin-top:10px;"
+          >
+            Add Discount
+          </p>
+          <form action="/staff/discounts.php" method="POST">
+              <input type="hidden" name="add" value="add"/>
+              <input
+                class="form-control"
+                type="text"
+                name="discount_name"
+                placeholder="Discount Name"
+                style="padding: 10px;"
+              />
+              <br />
+              <input
+                class="form-control"
+                type="number"
+                name="discount_percentage"
+                placeholder="Discount Percentage"
+                style="padding: 10px;"
+              />
+              <br />
+
+              <br /><br /><br />
+              <button
+                type="submit"
+                style="width: 100%; background-color: #242625; border-color:#242625; font-weight: bold; padding: 10px;"
+                class="btn btn-primary"
+              >
+                Add Discount
+              </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <?php
+      // Modals
+      if (!empty($_GET["name"])) generateModalsByName($_GET["name"], !empty($_GET["page"]) ? $_GET["page"] : 1);
+      else generateModals(!empty($_GET["page"]) ? $_GET["page"] : 1);
+    ?>
+  </body>
+</html>
+
       <button
         type="button"
         class="btn btn-primary"
@@ -91,5 +253,3 @@
         </div>
       </div>
     </div>
-  </body>
-</html>
