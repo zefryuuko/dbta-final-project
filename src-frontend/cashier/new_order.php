8cdfb4167865a6c249ca8b05d4e9b9a7764884d9
Zef <titotobing0511@gmail.com>
2020-01-05 23:12:51 +0700

    Implement basic add and delete function

--- src-frontend/cashier/new_order.php
+++ src-frontend/cashier/new_order.php
@@ -15,12 +15,81 @@
       include("../backend/staff.php");
       $staffName = getStaffByID($_COOKIE["id"])[0]["staff_name"];
     ?>
+    <script type="application/javascript">
+    function generateID(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+        // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    }
+    function populateItems() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var items = JSON.parse(this.responseText);
+                var tableBody = "";
+                items.forEach(element => {
+                    tableBody = tableBody + `<tr><td>${element["item_name"]}</td><td>${element["item_size"]}</td><td>Rp. ${element["item_price"]}</td><td><a onclick="addItem(${element["item_id"]})"><img src="/resources/plus.svg" style="width: 33%"/></a></td></tr>`
+                });
+                document.getElementById("itemsTable").innerHTML = tableBody;
+            }
+        }
+        xhttp.open("GET", "http://localhost:8081/item");
+        xhttp.send();
+    }
+
+    function populateItemsByName() {
+        if (document.getElementById("itemSearchBox").value == "") {
+            populateItems();
+            return;
+        }
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var items = JSON.parse(this.responseText);
+                var tableBody = "";
+                items.forEach(element => {
+                    tableBody = tableBody + `<tr><td>${element["item_name"]}</td><td>${element["item_size"]}</td><td>Rp. ${element["item_price"]}</td><td><a onclick="addItem(${element["item_id"]})"><img src="/resources/plus.svg" style="width: 33%"/></a></td></tr>`
+                });
+                document.getElementById("itemsTable").innerHTML = tableBody;
+            }
+        }
+        xhttp.open("GET", `http://localhost:8081/item?name=${document.getElementById("itemSearchBox").value}`);
+        xhttp.send();
+    }
+
+    function addItem(id){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var element = JSON.parse(this.responseText)[0];
+                var uid = generateID(8);
+                var tableRow = `<tr id="${uid}"><td>${element["item_name"]}</td><td>${element["item_size"]}</td><td>Rp. ${element["item_price"]}</td><td><a onclick="removeItem('${uid}')"><img src="/resources/cross.svg" style="width: 45%" /></a></td><td><a href="#"><img src="/resources/discount.svg" style="width: 45%"></a></td></tr>`
+                var formData = `<input type="hidden" name="item" id="${uid}" value="${element["item_id"]}"/>`;
+                document.getElementById("transactionTable").innerHTML += tableRow;
+                document.getElementById("orderDetails").innerHTML += formData;
+            }
+        }
+        xhttp.open("GET", `http://localhost:8081/item?id=${id}`);
+        xhttp.send();
+    }
+
+    function removeItem(uid) {
+        document.getElementById(uid).remove();  // Delete table entry
+        document.getElementById(uid).remove();  // Delete form entry
+    }
+    </script>
     </head>
 
-    <body onload="try{auth()}catch(e){}">
+    <body onload="try{auth()}catch(e){} populateItems()">
     <div class="container">
         <?php include("../components/navbar/navbar_cashier.php"); ?>
-
+        <div id="test"></div>
         <div class="bodyLeft" style="float:left; ">
         <p style="font-size:25px; margin-top:13px; font-weight: bold;">
                 Current Transaction
@@ -29,16 +98,14 @@
                 <table class="table" style="width: 550px; float: left;">
                     <thead class="thead-dark" style="font-size: 20px">
                         <tr>
-                            <th scope="col" style="width: 40px">#</th>
-                            <th scope="col" style="width: 80px">Item</th>
-                            <th scope="col" style="width: 50px">Quantity</th>
-                            <th scope="col" style="width: 70px">Size</th>
-                            <th scope="col" style="width: 140px">Price</th>
+                            <th scope="col" style="width: 200px">Item</th>
+                            <th scope="col" style="width: 100px">Size</th>
+                            <th scope="col" style="width: 120px">Price</th>
                             <th scope="col"></th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
-                    <tbody style="font-size: 18px">
+                    <tbody id="transactionTable" style="font-size: 18px">
                         <?php include("../components/modular/new_order.php"); ?>
                     </tbody>
                 </table>
@@ -49,7 +116,7 @@
 
             <div class="table-responsive">
             <table class="table" style="width: 550px; float: left;">
-                    <thead class="thead-dark">
+                    <tbody class="thead-dark">
                         <tr>
                             <th scope="col" style="width: 240px; font-size: 25px; font-weight: bold;">
                                 Sub Total:
@@ -59,45 +126,44 @@
                             </th>
                             <th scope="col"></th>
                         </tr>
-                    </thead>
+                    </tbody>
                 </table>
             </div>
             <!--Subtotal table-->
+            <form id="orderDetails">
+                <button type="button" onclick="" class="btn btn-light btn-primary btn-lg btn-outline-dark" style="width: 60%">
+                    Close Order
+                </button>
 
-            <button type="button" href="/cashier/checkout.php" class="btn btn-light btn-primary btn-lg btn-outline-dark" style="width: 60%">
-                Close Order
-            </a>
-
-            <button type="button" onclick="window.location.replace('/cashier/index.php')" class="btn btn-danger btn-lg" style="margin-left:22px; width: 35%">
-                Cancel Order
-            </a>
+                <button type="button" onclick="window.location.replace('/cashier/index.php')" class="btn btn-danger btn-lg" style="margin-left:22px; width: 35%">
+                    Cancel Order
+                </button>
+            </form>
         </div>
         <!--End of Left Body-->
 
         <div class="bodyRight" style="float:right;">
         <nav class="navbar navbar-light" style="font-size:25px; padding: 10px; font-weight: bold; ">
                 <a class="navbar-brand">Items</a>
-                <form class="form-inline" style="">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-light btn-outline-dark my-2 my-sm-0" type="submit">
+                <div class="form-inline" style="">
+                    <input class="form-control mr-sm-2" id="itemSearchBox" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-light btn-outline-dark my-2 my-sm-0" onclick="populateItemsByName()">
                         Search
                     </button>
-                </form>
+                </div>
             </nav>
 
             <div class="table-responsive">
-            <table class="table" style="width: 450px; float: left; margin-top: 5px;">
+            <table class="table" style="width: 550px; float: left; margin-top: 5px;">
                     <thead class="thead-dark" style="font-size: 20px">
                         <tr>
-                            <th scope="col" style="width: 100px">Item ID</th>
-                            <th scope="col" style="width: 100px">Item</th>
-                            <th scope="col" style="width: 150px">Price</th>
+                            <th scope="col" style="width: 200px">Item Name</th>
+                            <th scope="col" style="width: 150px">Size</th>
+                            <th scope="col" style="width: 120px">Price</th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
-                    <tbody style="font-size: 18px">
-                        <?php include("../components/modular/menu_order.php"); ?>
-                        <?php include("../components/modular/menu_order.php"); ?>
+                    <tbody id="itemsTable" style="font-size: 18px">
                         <?php include("../components/modular/menu_order.php"); ?>
                     </tbody>
                 </table>
