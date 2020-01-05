1f2ba9f32cf68e3be861177bbdcd2c693d7bf3b2
Zef <titotobing0511@gmail.com>
2020-01-05 16:10:17 +0700

    Show only bills made by specific cashier

--- src-frontend/components/modular/history_cashier.php
+++ src-frontend/components/modular/history_cashier.php
@@ -3,7 +3,7 @@ include("../backend/bill.php");
 
 function generateTableBody($currentPage = 1)
 {
-    $bills = getBills($currentPage);
+    $bills = getBillsByStaffID($_COOKIE["id"], $currentPage);
     foreach ($bills as $bill)
     {
         echo "<tr><td>".$bill["bill_id"]."</td><td>".$bill["branch_name"]."</td><td>".(($bill["staff_name"]))."</td><td>".$bill["check_number"]."</td><td><a href=\"/staff/transaction_details.php?id=".$bill["bill_id"]."\"><img src=\"/resources/view.svg\" style=\"width: 22%\" /></a></td></tr>";
@@ -22,7 +22,7 @@ function generatePagination($currentPage = 1)
         echo "<li class='page-item'><a class='page-link' href='"."/staff/history.php?page=".($currentPage - 1)."' tabindex='-1'>Back</a></li>";
     }
     echo "<li class=\"page-item active\"><span class=\"page-link\">Page ".$currentPage."</span></li>";
-    if (count(getBills($currentPage + 1)) == 0)
+    if (count(getBillsByStaffID($_COOKIE["id"], $currentPage + 1)) == 0)
     {
         echo "<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>Next</a></li>";
     }
